3fdb392fa84e092ba4ab8475226935e3054cee2c
Logan Hanks <loganhanks@mail.weber.edu>
2026-09-08 22:26:52 -0600

    Implemented rudimentary Input/Output UI as well as blueprint code to interact with the VM and display ports. When running the program, includes a text box for adding instructions manually as well as the run program button to execute the instructions.

--- Source/someAssemblyRequired/gameWidget_C.cpp
+++ Source/someAssemblyRequired/gameWidget_C.cpp
@@ -1,5 +1,46 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "gameWidget_C.h"
+#include "VM.h"
+
+void UgameWidget_C::NativeConstruct()
+{
+    UE_LOG(LogTemp, Warning, TEXT("GAME WIDGET NATIVE CONSTRUCT CALLED"));
+
+    VM = NewObject<UVM>(this);
+
+    //THESE TWO LINES CAN BE COMMENTED OUT WHEN IMPLEMENTING THE TEST CASES, THIS IS JUST SAMPLE DATA INJECTED INTO THE VM
+    TArray<FportDataLoader> preLoadedPorts;
+    preLoadedPorts.Add(FportDataLoader(0, { 2, 2 }));
+
+    VM->resetMachine(0, 1, preLoadedPorts);
+
+    UE_LOG(LogTemp, Warning, TEXT("P0 initialized with %d values"), VM->getPort(0).Num());
+
+    Super::NativeConstruct();
+}
+
+UVM* UgameWidget_C::GetVM() const
+{
+	return VM;
+}
+
+FText UgameWidget_C::PortValuesToText(const TArray<int32>& Values) const
+{
+    FString Result = TEXT("[");
+
+    for (int32 i = 0; i < Values.Num(); i++)
+    {
+        Result += FString::FromInt(Values[i]);
+
+        if (i < Values.Num() - 1)
+        {
+            Result += TEXT(", ");
+        }
+    }
+
+    Result += TEXT("]");
+
+    return FText::FromString(Result);
+}
 
--- Source/someAssemblyRequired/gameWidget_C.h
+++ Source/someAssemblyRequired/gameWidget_C.h
@@ -6,12 +6,28 @@
 #include "Blueprint/UserWidget.h"
 #include "gameWidget_C.generated.h"
 
+class UVM;
+
 /**
- * 
+ * Main game UI widget.
  */
 UCLASS()
 class SOMEASSEMBLYREQUIRED_API UgameWidget_C : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+protected:
+
+	virtual void NativeConstruct() override;
+
+	UPROPERTY()
+	UVM* VM;
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "VM")
+	UVM* GetVM() const;
+
+	UFUNCTION(BlueprintCallable, Category = "VM")
+	FText PortValuesToText(const TArray<int32>& Values) const;
 };
